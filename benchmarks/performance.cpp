ab18e5123b902fa1128345acb34cbbaf19739ecb
Tamara Mitrovska <tamaram@mit.edu>
2023-01-19 11:41:40 -0500

    added more timing examples from re2

--- benchmarks/performance.cpp
+++ benchmarks/performance.cpp
@@ -76,7 +76,28 @@ int all_matches_handler(unsigned int id, unsigned long long from,
     return 0;    
 }
 
-void time_compare(const vector<string> &patterns, const vector<string> &strings, int n_iters, MatchType match_type, vector<int> &n_funcs, vector<int> decompose) {
+// taken from https://github.com/google/re2/blob/main/re2/testing/regexp_benchmark.cc
+// Generate random text that won't contain the search string,
+// to test worst-case search behavior.
+std::string generate_random_text(int64_t nbytes) {
+    static const std::string* const text = []() {
+        std::string* text = new std::string;
+        srand(1);
+        text->resize(16<<20);
+        for (int64_t i = 0; i < 16<<20; i++) {
+            // Generate a one-byte rune that isn't a control character (e.g. '\n').
+            // Clipping to 0x20 introduces some bias, but we don't need uniformity.
+            int byte = rand() & 0x7F;
+            if (byte < 0x20)
+                byte = 0x20;
+            (*text)[i] = byte;
+        }
+        return text;
+    }();
+    return text->substr(0, nbytes);
+}
+
+void time_compare(const vector<string> &patterns, const vector<string> &strings, int n_iters, MatchType match_type, const vector<int> &n_funcs, const vector<int>& decompose) {
     // pattern compilation
     vector<MatchFunction**> buildit_patterns;
     vector<int> sub_parts; // sizes of the regex parts
@@ -133,7 +154,7 @@ void time_compare(const vector<string> &patterns, const vector<string> &strings,
         auto end = high_resolution_clock::now();
         cout << "buildit compile time: " << (duration_cast<nanoseconds>(end - start)).count() / 1e6f << "ms" << endl;
         buildit_patterns.push_back(all_funcs);
-        
+       
         // re2
         auto re_start = high_resolution_clock::now();
         re2_patterns.push_back(unique_ptr<RE2>(new RE2(patterns[i])));
@@ -250,7 +271,7 @@ void time_compare(const vector<string> &patterns, const vector<string> &strings,
             buildit_dur = (duration_cast<nanoseconds>(buildit_end - buildit_start)).count() * 1.0 / (1e6f * n_iters);
         }
         // check correctness
-		const string& str = (match_type == MatchType::PARTIAL_SINGLE) ? "<Twain Text>" : strings[j];
+		const string& str = (match_type == MatchType::PARTIAL_SINGLE) ? "<text>" : strings[j].substr(0, 10) + "...";
         
         if (!((hs_result == buildit_result || match_type == MatchType::FULL) && re_result == buildit_result)) {
             cout << "Correctness failed for regex " << patterns[j] << " and text " << str << ":" <<endl;
@@ -261,10 +282,10 @@ void time_compare(const vector<string> &patterns, const vector<string> &strings,
         }
 
         cout << "--- pattern: " << patterns[j] << " text: " << str << " ---" << endl;
+        cout << "buildit run time: " << buildit_dur << " ms" << endl;
         cout << "re2 run time: " << re2_dur << " ms" << endl;
         cout << "hs run time: " << hs_dur << " ms" << endl;
         cout << "pcre run time: " << pcre_dur << " ms" << endl;
-        cout << "buildit run time: " << buildit_dur << " ms" << endl;
     }
 
 }
@@ -272,6 +293,7 @@ void time_compare(const vector<string> &patterns, const vector<string> &strings,
 void optimize_partial_match_loop(string str, string pattern) {
     
     string re = expand_regex(pattern);
+    cout << "simple re " << re << endl;
     const int re_len = re.length();
     // precompute state transitions
     const int cache_size = (re_len + 1) * (re_len + 1); 
@@ -308,14 +330,11 @@ void optimize_partial_match_loop(string str, string pattern) {
 
 }
 
-int main() {
-    string patterns_file = "./data/twain_patterns.txt";
+void run_twain_benchmark() {
+    int n_iters = 10;
     string corpus_file = "./data/twain.txt";
     string text = load_corpus(corpus_file);
     std::cout << "Twain Text Length: " << text.length() << std::endl;
-
-    vector<string> patterns = load_patterns(patterns_file);
-    int n_iters = 10;
     vector<string> twain_patterns = {
         "Twain",
         "(Huck[a-zA-Z]+|Saw[a-zA-Z]+)",
@@ -333,12 +352,40 @@ int main() {
 	time_compare(twain_patterns, words, n_iters, MatchType::FULL, n_funcs, decompose);
 	cout << endl;
     time_compare(twain_patterns, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, n_funcs, decompose);
-    //cout << "pcre" << pcrecpp::RE("h.*o").FullMatch("hello") << endl;
+    
     // trying to optimize partial matches
     for (string re: twain_patterns) {
         cout << endl;
         cout << re << endl;
         optimize_partial_match_loop(text, re + ".*");
     }
+    
+}
+
+void run_re2_benchmark() {
+    // benchmark taken from https://swtch.com/~rsc/regexp/regexp3.html
+    int n_iters = 10;
+    string text = generate_random_text(1000000); 
+    // full match
+    time_compare(vector<string>{".*"}, vector<string>{text}, n_iters, MatchType::FULL, vector<int>{1}, vector<int>{0});
+    time_compare(vector<string>{"[0-9]+.(.*)"}, vector<string>{"650-253-0001"}, n_iters, MatchType::FULL, vector<int>{1}, vector<int>{0});
+    // partial match, no submatch
+    time_compare(vector<string>{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{1}, vector<int>{0});
+    time_compare(vector<string>{"[XYZ]ABCDEFGHIJKLMNOPQRSTUVWXYZ"}, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{1}, vector<int>{0});
+    time_compare(vector<string>{"A[AB]B[BC]C[CD]D[DE]E[EF]F[FG]G[GH]H[HI]I[IJ]J"}, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{1}, vector<int>{0});
+    time_compare(vector<string>{"A(A|B)B(B|C)C(C|D)D(D|E)E(E|F)F(F|G)G(G|H)H(H|I)I(I|J)J"}, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{1}, vector<int>{0});
+    time_compare(vector<string>{"[ -~]*ABCDEFGHIJKLMNOPQRSTUVWXYZ"}, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{1}, vector<int>{0});
+    time_compare(vector<string>{"([ -~])*(A)(B)(C)(D)(E)(F)(G)(H)(I)(J)(K)(L)(M)(N)(O)(P)(Q)(R)(S)(T)(U)(V)(W)(X)(Y)(Z)"}, vector<string>{text}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{1}, vector<int>{0});
+    // partial match, with submatch
+    //optimize_partial_match_loop(text + "650 253-0001", "(\\d{3}\\s+)(\\d{3}-\\d{4}).*");
+    //time_compare(vector<string>{"(\\d{3}\\s+)(\\d{3}-\\d{4})"}, vector<string>{text + "650 253-0001"}, n_iters, MatchType::PARTIAL_SINGLE, vector<int>{16}, vector<int>{0});
+
+}
+
+int main() {
+    string patterns_file = "./data/twain_patterns.txt";
+    vector<string> patterns = load_patterns(patterns_file);
+    //run_twain_benchmark();
+    run_re2_benchmark();
 }
 
